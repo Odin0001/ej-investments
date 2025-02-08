313178affc11fa8f72123305af8f15100f9e9209
Odin0001 <100428477+Odin0001@users.noreply.github.com>
2025-02-08 20:26:47 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -305,7 +305,7 @@ function Dashboard() {
           <h2 className="text-xl font-semibold mb-4 text-red-600 group-hover:text-white">Withdraw</h2>
           <p className="text-gray-600 group-hover:text-white">Send your wallet ID with your username and the amount of money you want to withdraw in the body of the email to our support team to verify your withdrawal <a href="mailto:support@ej-investments.info" className='underline'>here</a></p>
           <p className="text-gray-600 group-hover:text-white">PS: Our minimal withdrawal amount for each currency are as follow:</p>
-          <ul className="text-gray-600 group-hover:text-white text-wrap>
+          <ul className="text-gray-600 group-hover:text-white text-wrap">
             <li>- Usdt 10$</li>
             <li>- Trx 5$</li>
             <li>- Litecoin 1$</li>
